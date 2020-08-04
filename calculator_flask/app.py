eed7e3c26830e6f77c5fb202e35e888361a78888
Tony Chou <tcheon8788@gmail.com>
2020-08-04 15:58:02 +0800

    re init

---
+++ calculator_flask/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from utils.calculator import calculate 
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def calculator():
+    data = request.get_json()
+    ans = calculate(data['Num1'], data['OP'], data['Num2'])
+    return jsonify({
+        'ans': ans
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True) 
---
+++ calculator_flask/utils/calculator.py
@@ -0,0 +1,9 @@
+def calculate(num1, op, num2):
+    if op == "+":
+        return num1 + num2
+    elif op == "-":
+        return num1 - num2
+    elif op == "*":
+        return num1 * num2
+    elif op == "/":
+        return num1 / num2
